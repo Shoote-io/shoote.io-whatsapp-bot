040dbb9c9044dd8e0ed47cf83a0ac8f8b7992041
Elmidor Mario Rodriguez zagalo <shoote.io@elmidorgroup.com>
2025-12-01 06:51:09 +0100

    paj ofisyèl
    mete bon modèl groq

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ async function generateAIReply(prompt) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "mixtral-8x7b-32768",
+        model: "llama-3.1-70b-versatile",
         messages: [
           { role: "system", content: "Ou se yon asistan pwofesyonèl ki reponn trè klè." },
           { role: "user", content: prompt }
