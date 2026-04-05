2c74dbc698495804ab30185e5e4501b78d8c2b5e
Elmidor Mario Rodriguez zagalo <shoote.io@elmidorgroup.com>
2026-04-05 02:51:49 +0100

    elmidor os
    pou bon kòmand

--- index.js
+++ index.js
@@ -256,7 +256,7 @@ const ENGINE = {
 
     const payload = {
       action: "install_script",
-      name: "master-media",
+      name: "media-os",
       url: "https://raw.githubusercontent.com/Shoote-io/elmidor-toolkit-control/main/master-media.ps1",
       target: "tools",
       run_after: "media"
